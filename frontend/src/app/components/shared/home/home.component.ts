954bbe5586d23adafcdba4fddc2182c6c1362c00
victorpreston <prestonvictor25@gmail.com>
2024-10-12 16:27:36 +0300

    angular: shared components

---
+++ frontend/src/app/components/shared/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavbarComponent, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
