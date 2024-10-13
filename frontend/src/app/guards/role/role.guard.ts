8e0d451b16cde6e11d8f88c32658579065662bc7
victorpreston <prestonvictor25@gmail.com>
2024-10-13 18:39:40 +0300

    auth & role guard, core components: admin, company & jobseeker

---
+++ frontend/src/app/guards/role/role.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    const userRole = this.authService.getUserRole();
+
+    if (userRole === 'ADMIN') {
+      this.router.navigate(['/admin-dashboard']);
+      return true;
+    } else if (userRole === 'COMPANY') {
+      this.router.navigate(['/company-dashboard']);
+      return true;
+    } else if (userRole === 'JOBSEEKER') {
+      this.router.navigate(['/jobseeker-dashboard']);
+      return true;
+    } else {
+      this.router.navigate(['/home']);
+      return false;
+    }
+  }
+}
