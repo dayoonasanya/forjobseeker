e3144e69f838792e2a10991d447eb19798d0aa33
victorpreston <prestonvictor25@gmail.com>
2024-10-15 10:10:25 +0300

    user job applications && application profiles

--- frontend/src/app/components/shared/job-details/job-details.component.ts
+++ frontend/src/app/components/shared/job-details/job-details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink, Router } from '@angular/router';
 import { JobService } from '../../../services/job/job.service';
 import { Job } from '../../../interfaces/job';
+import { ApplicationService } from '../../../services/application/application.service';
+import { AuthService } from '../../../services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../navbar/navbar.component";
 
@@ -16,10 +18,14 @@ export class JobDetailsComponent implements OnInit {
   job: Job | null = null;
   isLoading: boolean = true;
   errorMessage: string = '';
+  notificationMessage: string = ''; 
 
   constructor(
     private jobService: JobService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private applicationService: ApplicationService,
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -43,6 +49,49 @@ export class JobDetailsComponent implements OnInit {
       }
     });
   }
+
+  applyForJob(): void {
+    const isLoggedIn = this.authService.isAuthenticated();
+  
+    if (!isLoggedIn) {
+      this.notificationMessage = 'Please log in to apply for this job.';
+      this.router.navigate(['/login']);
+    } else {
+      const jobId = this.job?.id;
+      const jobSeekerId = localStorage.getItem('jobseeker-id');
+  
+      console.log('JobSeeker ID:', jobSeekerId);
+  
+      if (!jobSeekerId) {
+        this.notificationMessage = 'No JobSeeker ID found. Please log in again.';
+        this.authService.logout();
+        return;
+      }
+  
+      if (jobId && jobSeekerId) {
+        const applicationData = {
+          jobId: jobId,
+          jobSeekerId: jobSeekerId,
+          status: 'PENDING'
+        };
+  
+        console.log('Application data to be sent:', applicationData);
+  
+        this.applicationService.createApplication(applicationData).subscribe({
+          next: (response) => {
+            console.log('Application submitted:', response);
+            this.notificationMessage = 'Your application has been submitted successfully!';
+          },
+          error: (error) => {
+            console.error('Error submitting application:', error);
+            this.notificationMessage = 'Failed to submit your application. Please try again later.';
+          }
+        });
+      }
+    }
+  }
+  
+  
 
   /**
    * Function to get formatted job type
